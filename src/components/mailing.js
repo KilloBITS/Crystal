47b0c78e869779e373cc62dfec55e0bb1aca4515
mr.kalinuk <qazwsx159357>
2019-07-13 12:46:25 +0300

    41

--- src/components/mailing.js
+++ src/components/mailing.js
@@ -1,26 +1,40 @@
 import React from 'react';
+import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class MailingBlock extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      sendingEmail: false,
       valueLine: ''
     }
   }
   handleSubmit(){
+    this.setState({
+      sendingEmail: true
+    })
     let myLocation = (window.location.hostname === 'localhost')? window.location.origin.split('3000')[0]+'5002':window.location.origin;
     axios.post(myLocation+'/saveEmail', {text: this.state}).then(res => {
       setTimeout(() => {
         this.setState({
+          sendingEmail: false,
           valueLine: ''
         })
-      },1000)
+      },2000)
       return false
     });
   }
+
+  handleMailingChange(event){
+    var text = event.target.value;
+    this.setState({
+      valueLine: text
+    })
+  }
   render() {
     return <div className="block mailing" id="MailingBlock">
+      <div className={(this.state.sendingEmail)?"mailingLoader":"mailingLoader hide"}></div>
       <div className="mailingBackground">
         <div className="backgroundImage"></div>
       </div>
@@ -29,8 +43,8 @@ class MailingBlock extends React.Component {
           Хочете бути вкурсі останніх новин та завжди отримувати їх першим,
           залиште нам свій адрес електронної пошти і ми будемо тримати вас вкурсі всіх останніх подій)
         </div>
-        <input type="text" className="mailingInput" value={this.state.valueLine} placeholder="Введіть ваш адрес електронної пошти"/>
-        <input type="button" className="mailingInput button" value="Отримувати новини"/>
+        <input type="text" className="mailingInput" value={this.state.valueLine} onChange={this.handleMailingChange.bind(this)} placeholder="Введіть ваш адрес електронної пошти"/>
+        <input type="button" className="mailingInput button" value="Отримувати новини" onClick={this.handleSubmit.bind(this)}/>
       </div>
       <div className="cristall"><FontAwesomeIcon icon={['fas', 'gem']} /></div>
     </div>
