10607804e6fba7293ebb92ff1015a5d72623a78e
mr.kalinuk <qazwsx159357>
2019-07-13 13:09:39 +0300

    44

--- src/components/about.js
+++ src/components/about.js
@@ -128,6 +128,19 @@ class AboutBlock extends React.Component {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 595.3 135.5" preserveAspectRatio="none"><path d="M595.3 135.5V5.5c-53.4-11.9-113-3.4-164 12.3-22.4 6.9-43.8 15.1-64.9 23.7-26.9 11-53.3 22.5-80.9 32.7-49.2 18.2-104.8 32.2-160.9 28.3C80.9 99.6 40.9 86 0 75.3v60.2h595.3zM0 135.5h595.3"></path></svg>
         </div>
       </div>
+
+      <ul className="circles">
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+       </ul>
     </div>
   }
 }
